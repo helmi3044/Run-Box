5c2ce74723a1c30044654b1a48e3da986edd7976
aliya-r <71702354+aliya-r@users.noreply.github.com>
2020-11-24 23:08:57 +0700

    update rintangan main.cpp
    nambahin animasi jatoh

--- RINTANGAN/main.cpp
+++ RINTANGAN/main.cpp
@@ -2,54 +2,63 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-void Rintangan(){
+float y_pos = 0.48f;
+float delta = 0.05f;
+bool toDown = true;
+
+void displayMe(void){
+    glClear(GL_COLOR_BUFFER_BIT);
     //Badan Misil
     glBegin(GL_POLYGON);
     glColor3ub(220, 220, 220);
-    glVertex2f(4,44);
-	glVertex2f(6,44);
-	glVertex2f(6,48);
-	glVertex2f(4,48);
+    glVertex2f(4, 44+ y_pos);
+	glVertex2f(6, 44+ y_pos);
+	glVertex2f(6, 48+ y_pos);
+	glVertex2f(4, 48+ y_pos);
     glEnd();
 
     //Sayap Kiri Misil
     glBegin(GL_POLYGON);
     glColor3ub(105, 105, 105);
-    glVertex2f(3, 49);
-	glVertex2f(4, 48);
-	glVertex2f(4, 44.5);
-	glVertex2f(3, 45);
+    glVertex2f(3, 49+ y_pos);
+	glVertex2f(4, 48+ y_pos);
+	glVertex2f(4, 44.5+ y_pos);
+	glVertex2f(3, 45+ y_pos);
     glEnd();
 
     //Sayap Kanan Misil
     glBegin(GL_POLYGON);
     glColor3ub(105, 105, 105);
-    glVertex2f(7, 49);
-	glVertex2f(6, 48);
-	glVertex2f(6, 44.5);
-	glVertex2f(7, 45);
+    glVertex2f(7, 49+ y_pos);
+	glVertex2f(6, 48+ y_pos);
+	glVertex2f(6, 44.5+ y_pos);
+	glVertex2f(7, 45+ y_pos);
     glEnd();
 
     //Moncong Misil
     glBegin(GL_POLYGON);
     glColor3ub(47, 79, 79);
-    glVertex2f(4, 44);
-	glVertex2f(4, 42.8);
-	glVertex2f(4.5, 42.3);
-	glVertex2f(5, 42);
-	glVertex2f(5.5, 42.3);
-	glVertex2f(6, 42.8);
-	glVertex2f(6, 44);
+    glVertex2f(4, 44+ y_pos);
+	glVertex2f(4, 42.8+ y_pos);
+	glVertex2f(4.5, 42.3+ y_pos);
+	glVertex2f(5, 42+ y_pos);
+	glVertex2f(5.5, 42.3+ y_pos);
+	glVertex2f(6, 42.8+ y_pos);
+	glVertex2f(6, 44+ y_pos);
     glEnd();
+    glutSwapBuffers();
 
 }
 
-void displayMe(void){
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    Rintangan();
+void timer(int) {
+    glutPostRedisplay();
+    glutTimerFunc(1000/30 , timer, 0);
+    if (y_pos < 43.5f && toDown)
+        y_pos -= delta;
+    else
+        toDown = false;
 
-    glutSwapBuffers();
 }
 
 int main(int argc, char** argv){
@@ -60,6 +69,7 @@ int main(int argc, char** argv){
 	glutCreateWindow("RINTANGAN"); //MEMBUAT WINDOW
 	glutDisplayFunc(displayMe); //MEMANGGIL DISPLAY
 	gluOrtho2D(0, 50, 0, 50); //MENGATUR JANGKAUAN TITIK PADA DISPLAY
+	glutTimerFunc(0, timer, 0);
 	glutMainLoop(); //PENGULANGAN
 	return 0;
 }
