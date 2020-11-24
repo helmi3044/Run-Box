19204fdabd9bac7a93b40fbcc4d6d60f6946f06f
helmi3044 <helmimansur88@gmail.com>
2020-11-24 22:30:43 +0700

    Update main.cpp

--- Kotak Keranjang Bola/main.cpp
+++ Kotak Keranjang Bola/main.cpp
@@ -12,93 +12,47 @@ void displayMe(void){
     glBegin(GL_POLYGON);//awal polygon
     glColor3f(0.6,0.3,0.0);//pemberian warna polygon kotak keranjang (warna coklat)
     glVertex2f(0 + x_pos, 0);
-    glVertex2f(0 + x_pos, 4);
-    glVertex2f(4 + x_pos, 4);
+    glColor3f(0.65,0.35,0.0);
+    glVertex2f(0 + x_pos, 6);
+    glColor3f(0.7,0.4,0.0);
     glVertex2f(6 + x_pos, 6);
-    glVertex2f(6 + x_pos, 2);
-    glVertex2f(4 + x_pos, 0);
-    glEnd();//akhir
-
-    glBegin(GL_POLYGON);//awal polygon
-    glColor3f(1.0,1.0,1.0);//pemberian warna polygon kotak keranjang (warna putih)
-    glVertex2f(0 + x_pos, 4);
-    glVertex2f(2 + x_pos, 6);
-    glVertex2f(6 + x_pos, 6);
-    glVertex2f(4 + x_pos, 4);
+    glColor3f(0.75,0.45,0.0);
+    glVertex2f(6 + x_pos, 0);
     glEnd();//akhir
 
     //membuat garis kotak keranjang
     glBegin(GL_LINES);//awal garis
     glColor3f (1.0,0.0,0.0);//pemberian warna garis kotak keranjang
     glVertex2f(0 + x_pos, 0);
-    glVertex2f(0 + x_pos, 4);
+    glVertex2f(0 + x_pos, 6);
     glEnd();//akhir
 
     //membuat garis kotak keranjang
     glBegin(GL_LINES);//awal garis
     glColor3f (1.0,0.0,0.0);//pemberian warna garis kotak keranjang
-    glVertex2f(0 + x_pos, 4);
-    glVertex2f(4 + x_pos, 4);
-    glEnd();//akhir
-
-    //membuat garis kotak keranjang
-    glBegin(GL_LINES);//awal garis
-    glColor3f (1.0,0.0,0.0);//pemberian warna garis kotak keranjang
-    glVertex2f(4 + x_pos, 4);
-    glVertex2f(4 + x_pos, 0);
-    glEnd();//akhir
-
-    //membuat garis kotak keranjang
-    glBegin(GL_LINES);//awal garis
-    glColor3f (1.0,0.0,0.0);//pemberian warna garis kotak keranjang
-    glVertex2f(0 + x_pos, 0);
-    glVertex2f(4 + x_pos, 0);
-    glEnd();//akhir
-
-    //membuat garis kotak keranjang
-    glBegin(GL_LINES);//awal garis
-    glColor3f (1.0,0.0,0.0);//pemberian warna garis kotak keranjang
-    glVertex2f(4 + x_pos, 0);
-    glVertex2f(6 + x_pos, 2);
-    glEnd();//akhir
-
-    //membuat garis kotak keranjang
-    glBegin(GL_LINES);//awal garis
-    glColor3f (1.0,0.0,0.0);//pemberian warna garis kotak keranjang
-    glVertex2f(6 + x_pos, 2);
+    glVertex2f(6 + x_pos, 0);
     glVertex2f(6 + x_pos, 6);
     glEnd();//akhir
 
     //membuat garis kotak keranjang
     glBegin(GL_LINES);//awal garis
-    glColor3f (1.0,0.0,0.0);//pemberian warna garis kotak keranjang
+    glColor3f (1.0,0.0,0.0);//pemberian warna ungu pagar
+    glVertex2f(0 + x_pos, 6);
     glVertex2f(6 + x_pos, 6);
-    glVertex2f(4 + x_pos, 4);
-    glEnd();//akhir
-
-    //membuat garis kotak keranjang
-    glBegin(GL_LINES);//awal garis
-    glColor3f (1.0,0.0,0.0);//pemberian warna garis kotak keranjang
-    glVertex2f(6+ x_pos, 6);
-    glVertex2f(2 + x_pos, 6);
-    glEnd();//akhir
-
-    //membuat garis kotak keranjang
-    glBegin(GL_LINES);//awal garis
-    glColor3f (1.0,0.0,0.0);//pemberian warna garis kotak keranjang
-    glVertex2f(2 + x_pos, 6);
-    glVertex2f(0 + x_pos, 4);
     glEnd();//akhir
 
     //membuat motif kotak
     glBegin(GL_POLYGON);//awal polygon
     glColor3f(0.6,0.0,0.0);//pemberian warna polygon motif kotak
-    glVertex2f(2 + x_pos, 1);
-    glVertex2f(1 + x_pos, 2);
-    glVertex2f(2 + x_pos, 3);
-    glVertex2f(3 + x_pos, 2);
+    glVertex2f(3 + x_pos, 1);
+    glColor3f(0.8,0.0,0.0);
+    glVertex2f(1 + x_pos, 3);
+    glColor3f(1.0,0.0,0.0);
+    glVertex2f(3 + x_pos, 5);
+    glColor3f(1.2,0.0,0.0);
+    glVertex2f(5 + x_pos, 3);
     glEnd();//akhir
-    glutSwapBuffers();
+	glutSwapBuffers();
 }
 
 void timer(int) {
